8c66ed185947ef44c0bbfae0f0d813d1b066cca9
Avery Zavoda <avery.zavoda@gmail.com>
2026-06-11 20:32:07 +1200

    Add prerendering

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,7 +14,14 @@ export default defineNuxtConfig({
     branch: 'main'
   }
   },
-
+  nitro: {
+    prerender: {
+      // Pre-render the homepage
+      routes: ['/'],
+      // Then crawl all the links on the page
+      crawlLinks: true
+    }
+  },
   devtools: {
     enabled: true
   },
