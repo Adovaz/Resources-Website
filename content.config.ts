f7ca68ed3a6faf822d441152231fa918a2b76f0c
Elizabeth Urquhart <me@ell.gay>
2026-06-15 10:37:26 +1200

    chore: restructure nuxt content

--- content.config.ts
+++ content.config.ts
@@ -1,23 +1,23 @@
 import { defineCollection, defineContentConfig, z } from '@nuxt/content'
 
+export const Resource = z.object({
+  title: z.string(),
+  description: z.string(),
+  image: z.string().editor({ input: 'media' }),
+  // Can't use .editor({ input: 'media' }) for the file selector unfortunately,
+  // as Nuxt Studio only seems to support media files (not PDF!)
+  file: z.string()
+})
+
 export default defineContentConfig({
   collections: {
-    index: defineCollection({
+    content: defineCollection({
       type: 'page',
       source: '**/*.yml',
       schema: z.object({
         title: z.string(),
-        description: z.string()
-      })
-    }),
-    resources: defineCollection({
-      type: 'data',
-      source: 'resources/**.yml',
-      schema: z.object({
-        title: z.string(),
         description: z.string(),
-        image: z.string(),
-        link: z.string()
+        resources: z.array(Resource)
       })
     })
   }
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,41 +1,48 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/eslint", "@nuxt/ui", "@nuxt/content", "nuxt-studio"],
+  modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxt/content', 'nuxt-studio'],
   devtools: {
-    enabled: false,
+    enabled: true
   },
 
-  css: ["~/assets/css/main.css"],
+  css: ['~/assets/css/main.css'],
 
   routeRules: {
-    "/": { prerender: true },
+    '/': { prerender: true }
   },
 
-  compatibilityDate: "2025-01-15",
+  compatibilityDate: '2025-01-15',
   nitro: {
     prerender: {
       // Pre-render the homepage
-      routes: ["/"],
+      routes: ['/'],
       // Then crawl all the links on the page
-      crawlLinks: true,
+      crawlLinks: true
     },
+
+    preset: 'cloudflare_module',
+
+    cloudflare: {
+      deployConfig: true,
+      nodeCompat: true
+    }
   },
 
   eslint: {
     config: {
       stylistic: {
-        commaDangle: "never",
-        braceStyle: "1tbs",
-      },
-    },
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
   },
 
   studio: {
     repository: {
-      provider: "github", // 'github' or 'gitlab'
-      owner: "Adovaz",
-      repo: "Resources-Website",
-      branch: "master",
-    },
-  },
-});
+      provider: 'github', // 'github' or 'gitlab'
+      owner: 'Adovaz',
+      repo: 'Resources-Website',
+      branch: 'master'
+    }
+  }
+})
